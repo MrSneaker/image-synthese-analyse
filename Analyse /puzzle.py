ab572d8f810c9c01189608c718eb93069980a3ea
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2023-04-03 16:47:31 +0200

    MAJ Fichier
    Ajout de la fonction permettant d'isoler les pièces de puzzle en les
    coloriant avec des couleurs différentes.
    MAJ avec l'utilisation d'une norme LAB et non HSV qui est plus
    précise.
    Rajout de la meilleur config pour la norme LAB.

--- Analyse /puzzle.py
+++ Analyse /puzzle.py
@@ -5,10 +5,13 @@ import re
 from collections import Counter
 
 
-imageCouleurRGB=cv2.imread('exemple_jouets.jpeg')
+imageCouleurRGB=cv2.imread('exemple_puzzle1.jpeg')
 
 imageCouleurHSV=cv2.cvtColor(imageCouleurRGB,cv2.COLOR_BGR2HSV)
+imageGreyScale=cv2.cvtColor(imageCouleurRGB,cv2.COLOR_BGR2GRAY)
+imageCouleurLAB=cv2.cvtColor(imageCouleurRGB,cv2.COLOR_BGR2LAB)
 imageCouleurHSVGauss=cv2.GaussianBlur(imageCouleurHSV,(5,5),0)
+imageCouleurLABGauss=cv2.GaussianBlur(imageCouleurLAB,(5,5),0)
 
 longueurImage=imageCouleurHSVGauss.shape[1]
 largeurImage=imageCouleurHSVGauss.shape[0]
@@ -88,29 +91,71 @@ def contourFormeColonne(image, colonne):
     for i in range (0,len(tabCouleurPixel)-1):
         #si le pixel est d'une couleur différente au pixel précédent
         if(tabCouleurPixel[i][0]!=tabCouleurPixel[i+1][0]):
-            #le pixel de l'image correspondant à un contour devient rouge
-            image[i][colonne]=[0,0,255]
-    
+            #le pixel de l'image correspondant à un contour devient cyan
+            image[i][colonne]=[255,255,0]
+
+
+
+
+
+#---------------------------------------Isolement d'une pièce du puzzle-------------------------------------------- 
+def isolementPiecePuzzle(image):
+    # Inverser les couleurs de l'image
+    inv_image = cv2.bitwise_not(image)
+    #conversion en niveaux de gris
+    gray = cv2.cvtColor(inv_image, cv2.COLOR_BGR2GRAY)
+    # Détection des composantes connexes
+    num_labels, labels = cv2.connectedComponents(gray)
+
+    # generer une couleur differente pour chaque composante connexe
+    colors = np.random.randint(0, 256, size=(num_labels, 3), dtype=np.uint8)
+
+    # Colorier chaque composante connexe avec une couleur différente
+    colored = np.zeros_like(image)
+    for label in range(1, num_labels):
+        mask = labels == label
+        colored[mask] = colors[label]
+    return colored
+
+
+
+
+            
+
+
 
 
 #---------------------------------------Appel des fonctions--------------------------------------------
 
+
+erreur=15
+
 # Définition de la couleur dominante de l'image imageCouleurHSVGauss
-CouleurDominanteHSV=plageDeCouleurZoneInterressante(imageCouleurHSVGauss,0,largeurImage,0,longueurImage)
+CouleurDominanteHSV=plageDeCouleurZoneInterressante(imageCouleurLABGauss,0,largeurImage,0,longueurImage)
 
 # Transformation de l'image couleur en image noir et blanc
-HSVtoBW(imageCouleurHSVGauss,CouleurDominanteHSV, 70)
+HSVtoBW(imageCouleurLABGauss,CouleurDominanteHSV, erreur)
 
+#kernel 
+kernel = np.ones((3,3),np.uint8)
+imageCouleurLABGauss=cv2.morphologyEx(imageCouleurLABGauss, cv2.MORPH_OPEN, kernel)
 
 #contour de la forme (parcours en largeur et en hauteur de l'image => plus de précision dans le contour)
-for i in range (imageCouleurHSVGauss.shape[0]):
-    contourFormeLigne(imageCouleurHSVGauss,i)
+# for i in range (imageCouleurLABGauss.shape[0]):
+#     contourFormeLigne(imageCouleurLABGauss,i)
 
-for i in range (imageCouleurHSVGauss.shape[1]):
-    contourFormeColonne(imageCouleurHSVGauss,i)
+# for i in range (imageCouleurLABGauss.shape[1]):
+#     contourFormeColonne(imageCouleurLABGauss,i)
+
+#isolement d'une pièce du puzzle
+imageCouleurLABGaussPieceColoree=isolementPiecePuzzle(imageCouleurLABGauss)
 
 
-
-cv2.imshow("Image Noir et Blanc",imageCouleurHSVGauss)
+cv2.imshow("Image Noir et Blanc",imageCouleurLABGauss)
+cv2.imshow("Image Pieces Coloree",imageCouleurLABGaussPieceColoree)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+#best config : 
+#imageCouleurLABGauss, erreur = 15 , kernel : (3,3), parcours sur L , A et B (indice 0,1,2)
